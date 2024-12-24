df05e4d00adc584da92b28fd8cf7499ff124bb5f
Afi Naufal <afinaufal@Afis-MacBook-Air.local>
2024-12-24 15:56:33 +0700

    add filter

--- controller/get_book.go
+++ controller/get_book.go
@@ -3,12 +3,33 @@ package controller
 import (
 	"api_go_bwa/database"
 	"api_go_bwa/dto"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"net/http"
+	"strings"
 )
 
 func GetAllBooks(c *fiber.Ctx) error {
-	rows, err := database.DB.Query("SELECT * FROM books")
+
+	var request dto.RequestBook
+	err := c.QueryParser(&request)
+	//
+	query := "SELECT * FROM books"
+	if err != nil {
+		return err
+	}
+	//
+	println(request.Search)
+	//
+	var arg []any
+	if request.Search != "" {
+		println("masuk sini")
+		query += " WHERE LOWER(title) LIKE $1 OR LOWER(author) LIKE $1 OR LOWER(description) LIKE $1 OR LOWER(genre) LIKE $1 "
+		filter := fmt.Sprintf("%%%s%%", strings.ToLower(request.Search))
+		arg = append(arg, filter)
+	}
+
+	rows, err := database.DB.Query(query, arg...)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
@@ -28,10 +49,19 @@ func GetAllBooks(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	return c.Status(http.StatusOK).JSON(
+	if books != nil {
+		return c.Status(http.StatusOK).JSON(
+			map[string]any{
+				"status":  http.StatusOK,
+				"message": "success",
+				"data":    books,
+			})
+	}
+
+	return c.Status(http.StatusNotFound).JSON(
 		map[string]any{
-			"status":  http.StatusOK,
-			"message": "success",
-			"data":    books,
+			"status":  http.StatusNotFound,
+			"message": "Data tidak ditemukan",
 		})
+
 }
---
+++ dto/req_book.go
@@ -0,0 +1,5 @@
+package dto
+
+type RequestBook struct {
+	Search string `query:"search"`
+}
